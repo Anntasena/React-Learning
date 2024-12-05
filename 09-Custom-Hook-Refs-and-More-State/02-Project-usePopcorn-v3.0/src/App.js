f3f5074e82780d6de20a08a51c29df5764956011
Syahrin Matlail <matlailsyahrin@gmail.com>
2024-12-05 16:45:14 +0700

    update learning react DAY-22

--- 09-Custom-Hook-Refs-and-More-State/02-Project-usePopcorn-v3.0/src/App.js
+++ 09-Custom-Hook-Refs-and-More-State/02-Project-usePopcorn-v3.0/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StartRating";
 
 const average = (arr) =>
@@ -167,6 +167,34 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  //$ Cara menggunakan useRef
+  //= 1. definisikan
+  const inputEl = useRef(null);
+
+  //= 3. menggunakan useEffect dan mengubahnya sesuai yang diinginkan
+  useEffect(
+    function () {
+      const callback = function (e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      };
+
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
+  // useEffect(function () {
+  //   const el = document.querySelector(".search");
+  //   console.log(el);
+  //   el.focus();
+  // }, []);
+
   return (
     <input
       className="search"
@@ -174,6 +202,8 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      //= 2. memasangnya ke element yang ingin di target
+      ref={inputEl}
     />
   );
 }
@@ -233,6 +263,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const countRef = useRef(0);
+
+  useEffect(
+    function () {
+      if (userRating) countRef.current += 1;
+    },
+    [userRating]
+  );
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
@@ -277,6 +316,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
 
     onAddWatched(newWatchMovie);
