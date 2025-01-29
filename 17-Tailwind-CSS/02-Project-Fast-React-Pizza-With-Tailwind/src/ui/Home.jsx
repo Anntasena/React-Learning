f0d4f81ae76c8dfaae746d1fefb5d406c1f86841
Syahrin Matlail <matlailsyahrin@gmail.com>
2025-01-29 20:09:40 +0700

    update learning react DAY-53

--- 17-Tailwind-CSS/02-Project-Fast-React-Pizza-With-Tailwind/src/ui/Home.jsx
+++ 17-Tailwind-CSS/02-Project-Fast-React-Pizza-With-Tailwind/src/ui/Home.jsx
@@ -1,11 +1,17 @@
+import CreateUser from "../features/user/CreateUser";
+
 function Home() {
   return (
-    <div>
-      <h1 className="text-xl font-semibold text-yellow-500">
+    <div className="mb-10 mt-8 px-4 text-center sm:my-16">
+      <h1 className="font-xl mb-8 font-semibold md:text-3xl">
         The best pizza.
         <br />
-        Straight out of the oven, straight to you.
+        <span className="text-yellow-500">
+          Straight out of the oven, straight to you.
+        </span>
       </h1>
+
+      <CreateUser />
     </div>
   );
 }
