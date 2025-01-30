6d0671d044cdb370aae3b952dafc361f39bcfede
Syahrin Matlail <matlailsyahrin@gmail.com>
2025-01-30 18:52:26 +0700

    update learning react DAY-54

--- 17-Tailwind-CSS/02-Project-Fast-React-Pizza-With-Tailwind/src/ui/Button.jsx
+++ 17-Tailwind-CSS/02-Project-Fast-React-Pizza-With-Tailwind/src/ui/Button.jsx
@@ -1,18 +1,25 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className =
-    "inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+function Button({ children, disabled, to, type = "primary" }) {
+  const base =
+    "inline-block font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
+
+  const styles = {
+    primary: base + "px-4 py-3 md:px-6 md:py-4",
+    small: base + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "inline-block rounded-full border-2 border-stone-300 px-4 py-2.5 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-3.5",
+  };
 
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
--- 17-Tailwind-CSS/02-Project-Fast-React-Pizza-With-Tailwind/tailwind.config.js
+++ 17-Tailwind-CSS/02-Project-Fast-React-Pizza-With-Tailwind/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+// //= jika ingin mematikan eslint
+// eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    //= DEFAULT FONT => jika ingin merubah default font
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+
+    extend: {
+      //= EXTENDED COLOR => jika ingin menambahkan warna
+      colors: {
+        pizza: "#123451",
+      },
+      //= EXTENDED HEIGHT => merubah default satuan height
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
