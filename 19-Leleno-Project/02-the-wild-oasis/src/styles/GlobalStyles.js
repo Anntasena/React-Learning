46656b83f30a8644891a2afe610c503a8a71985f
Syahrin Matlail <matlailsyahrin@gmail.com>
2025-02-06 21:49:59 +0700

    update learning react DAY-59

--- 19-Leleno-Project/02-the-wild-oasis/src/styles/GlobalStyles.js
+++ 19-Leleno-Project/02-the-wild-oasis/src/styles/GlobalStyles.js
@@ -1,32 +1,7 @@
-/* 
-= ====================================================
-! ====================================================
-? ====================================================
-# FILE INI HANYA REFRENSI TIDAK ADA FUNGSINYA ========
-# FILE INI HANYA REFRENSI TIDAK ADA FUNGSINYA ========
-# FILE INI HANYA REFRENSI TIDAK ADA FUNGSINYA ========
-# FILE INI HANYA REFRENSI TIDAK ADA FUNGSINYA ========
-? ====================================================
-! ====================================================
-= ====================================================
-*/
+import { createGlobalStyle } from "styled-components";
 
-/*
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-  rel="stylesheet"
-/>
-<link
-  href="https://fonts.googleapis.com/css2?family=Sono:wght@400;500;600&display=swap"
-  rel="stylesheet"
-/>
-*/
-
-/* Colors adapted from https://tailwindcss.com/docs/customizing-colors */
-
-:root {
+const GlobalStyles = createGlobalStyle`
+  :root {
   /* Indigo */
   --color-brand-50: #eef2ff;
   --color-brand-100: #e0e7ff;
@@ -207,3 +182,6 @@ FOR DARK MODE
 --image-grayscale: 10%;
 --image-opacity: 90%;
 */
+`;
+
+export default GlobalStyles;
